8aef2e36b6ad6111e67e4ab9a4105c730ce81e20
Jean Max Mezalon <+>
2021-10-13 10:08:13 -0400

    commit tree ec0da5309eb4f920e0210449eb8dd4e362fbecc4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+const saturdayFun = (activity = 'roller-skate') => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = 'go to the office') => {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (character="*") => {
+    function result (adjective) {
+        return `You are ${character}${adjective}${character}!`
+    }
+    return result;
+}
